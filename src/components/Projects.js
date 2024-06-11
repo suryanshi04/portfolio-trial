652fd984dad977715dd972348db3a32a411d2e5b
kimiko <suryanshi180504@gmail.com>
2024-06-11 18:43:54 +0530

    project page update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,77 @@
 import React from 'react';
+import '../Styles/projects.css';
 
 const Projects = () => {
   return (
     <div>
-      <h1>My Projects</h1>
-      <p>This is the projects page.</p>
+      <center><h1 className='pp'>My Projects</h1>
+      <table>
+        <tr>
+          <td>
+            <div class="card">
+              <div class="first-content">
+                <span>First</span>
+              </div>
+              <div class="second-content">
+                <span>Second</span>
+              </div>
+            </div>
+          </td>
+          <td>
+            <div class="card">
+              <div class="first-content">
+                <span>First</span>
+              </div>
+              <div class="second-content">
+                <span>Second</span>
+              </div>
+            </div>
+          </td>
+          <td>
+            <div class="card">
+              <div class="first-content">
+                <span>First</span>
+              </div>
+              <div class="second-content">
+                <span>Second</span>
+              </div>
+            </div>
+          </td>
+        </tr>
+        <tr>
+        <td>
+            <div class="card">
+              <div class="first-content">
+                <span>First</span>
+              </div>
+              <div class="second-content">
+                <span>Second</span>
+              </div>
+            </div>
+          </td>
+          <td>
+            <div class="card">
+              <div class="first-content">
+                <span>First</span>
+              </div>
+              <div class="second-content">
+                <span>Second</span>
+              </div>
+            </div>
+          </td>
+          <td>
+            <div class="card">
+              <div class="first-content">
+                <span>First</span>
+              </div>
+              <div class="second-content">
+                <span>Second</span>
+              </div>
+            </div>
+          </td>
+        </tr>
+      </table>
+      </center>
     </div>
   );
 };
